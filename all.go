75743f667d6fc19ae4eadd54f2ee38c6d3bc6020
alvii147 <alvii147@gmail.com>
2024-11-03 12:23:31 -0500

    add iter.Seq hyperlink for All godoc

--- all.go
+++ all.go
@@ -4,7 +4,7 @@ import (
 	"iter"
 )
 
-// All computes whether or not all values in an iter.Seq sequence are
+// All computes whether or not all values in an [iter.Seq] sequence are
 // true.
 func All(seq iter.Seq[bool]) bool {
 	for value := range seq {
