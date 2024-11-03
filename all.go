f02443bcd6091e643f7c12b1275de67ec1d3ac17
alvii147 <alvii147@gmail.com>
2024-11-03 12:30:25 -0500

    use iter.Seq link in All

--- all.go
+++ all.go
@@ -6,8 +6,6 @@ import (
 
 // All computes whether or not all values in an [iter.Seq] sequence are
 // true.
-//
-// [iter.Seq]: https://pkg.go.dev/iter#Seq
 func All(seq iter.Seq[bool]) bool {
 	for value := range seq {
 		if !value {
